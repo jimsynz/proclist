245b9528dbf71192d9719f33d3c1f0ab479c3f8a
James Harton <james@harton.nz>
2025-04-23 21:03:40 +1200

    improvement: Use `RUSTLER_TARGET`.

--- lib/proclist.ex
+++ lib/proclist.ex
@@ -2,7 +2,7 @@ defmodule Proclist do
   @moduledoc """
   A wrapper around the `proclist` Rust crate which provides a list of operating system processes.
   """
-  use Rustler, otp_app: :proclist, crate: :proclist
+  use Rustler, otp_app: :proclist, crate: :proclist, target: System.get_env("RUSTLER_TARGET")
 
   @doc """
   Return a list of all processes currently running on the host operating system.
